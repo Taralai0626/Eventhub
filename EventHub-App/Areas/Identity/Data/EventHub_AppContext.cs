88b1babc4a3634adff78876832f2936d01198491
Tara Lai <taralai.developer@gmail.com>
2022-10-16 21:12:38 -0700

    Tara

---
+++ EventHub-App/Areas/Identity/Data/EventHub_AppContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace EventHub_App.Data;
+
+public class EventHub_AppContext : IdentityDbContext<IdentityUser>
+{
+    public EventHub_AppContext(DbContextOptions<EventHub_AppContext> options)
+        : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        // Customize the ASP.NET Identity model and override the defaults if needed.
+        // For example, you can rename the ASP.NET Identity table names and more.
+        // Add your customizations after calling base.OnModelCreating(builder);
+    }
+}
